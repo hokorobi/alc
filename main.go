43386c8a59cbdf883f0b8f7253a7a0d1e4ce31d6
nocd5 <nocd5rd@gmail.com>
2017-02-10 10:27:08 +0900

    Remove unused index

--- main.go
+++ main.go
@@ -72,7 +72,7 @@ func main() {
 		panic(err)
 	}
 
-	doc.Find(".ex_sentence, .kana").Each(func(i int, ex *goquery.Selection) {
+	doc.Find(".ex_sentence, .kana").Each(func(_ int, ex *goquery.Selection) {
 		ex.Remove()
 	})
 
@@ -88,12 +88,12 @@ func main() {
 	if err != nil {
 		panic(err)
 	}
-	doc.Find("#resultsList").Each(func(i int, result *goquery.Selection) {
-		result.Find(".midashi, .midashi_je").Each(func(_i int, midashi *goquery.Selection) {
-			if begin <= _i+1 && _i+1 <= end {
+	doc.Find("#resultsList").Each(func(_ int, result *goquery.Selection) {
+		result.Find(".midashi, .midashi_je").Each(func(i int, midashi *goquery.Selection) {
+			if begin <= i+1 && i+1 <= end {
 				fmt.Fprintln(stdOut, RED+midashi.Text()+END)
 				content := midashi.Next()
-				content.Find(".attr").Each(func(i int, attr *goquery.Selection) {
+				content.Find(".attr").Each(func(_ int, attr *goquery.Selection) {
 					str := rxlvl.ReplaceAllString(attr.Text(), "")
 					str = rxkana.ReplaceAllString(str, "【カナ】")
 					fmt.Fprintln(stdOut, YELLOW+str+END)
@@ -101,7 +101,7 @@ func main() {
 				if content.Is("div") {
 					wordclass := content.Find(".wordclass")
 					if wordclass.Size() > 0 {
-						wordclass.Each(func(__i int, w *goquery.Selection) {
+						wordclass.Each(func(_ int, w *goquery.Selection) {
 							fmt.Fprintln(stdOut, BLUE+w.Text()+END)
 							displayContent(stdOut, w.Next())
 						})
@@ -118,10 +118,10 @@ func main() {
 		})
 	})
 
-	doc.Find("#sas_word").Each(func(i int, sas *goquery.Selection) {
+	doc.Find("#sas_word").Each(func(_ int, sas *goquery.Selection) {
 		fmt.Fprintln(stdOut, "Suggestion:")
 		l := []string{}
-		sas.Find("span > a").Each(func(_i int, w *goquery.Selection) {
+		sas.Find("span > a").Each(func(_ int, w *goquery.Selection) {
 			l = append(l, w.Text())
 		})
 		fmt.Fprintln(stdOut, "\t"+BLUE+strings.Join(l, ", ")+END)
